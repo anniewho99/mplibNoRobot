e87927a4745ba67be13a1537273809d8b5b51372
anniewho99 <xinyuehu1999@gmail.com>
2025-01-21 15:13:13 -0800

    new condiiton

--- examples/baseline/baseline.js
+++ examples/baseline/baseline.js
@@ -1909,9 +1909,9 @@ async function initGame() {
       introName = name;
       // Define possible trap schedules
       const trapSchedules = [
-        { 1: [1], 2: [1], 3: [2], 4: [2] },  // First trap schedule
-        { 1: [1], 2: [2], 3: [2], 4: [1] },  // Second trap schedule
-        { 1: [1], 2: [2], 3: [1], 4: [2] }   // Third trap schedule
+        { 1: [1], 2: [1], 3: [1], 4: [1] },  // First trap schedule
+        { 1: [1], 2: [1], 3: [1], 4: [1] },  // Second trap schedule
+        { 1: [1], 2: [1], 3: [1], 4: [1]  }   // Third trap schedule
       ];
   
       // Randomly select one of the trap schedules
